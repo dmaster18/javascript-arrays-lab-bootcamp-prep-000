157d7e7a8c06f70eb863e7fced39cf684102cfcd
David Strauch <dmaster18@gmail.com>
2019-06-11 18:49:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  var newKittens = kittens.slice(0, kittens.length - 2);
+  var newKittens = kittens.slice(0, kittens.length - 1);
   return newKittens;
 }
 
